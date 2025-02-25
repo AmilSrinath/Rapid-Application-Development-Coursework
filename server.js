3d034280c6f5661f9df89e325c63244f6f6acd6c
AmilSrinath <amilsrinath5@gmail.com>
2025-02-25 21:55:53 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log("MongoDB Connected"));
+
+app.listen(5000, () => console.log('Server running on port 5000'));
